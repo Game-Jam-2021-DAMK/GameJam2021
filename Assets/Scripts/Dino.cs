3e3a4a3e19ee771cb52169e23550b6864c021b2b
Kyle Dixon <90637263+KyleDixonVO@users.noreply.github.com>
2021-11-20 17:54:41 -0400

    Dino V1.0
    Dino detects player on collision with a boxcollider. Dino then rushes the player and will kill them on contact.

---
+++ Assets/Scripts/Dino.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dino : MonoBehaviour
+{
+    public Rigidbody rb;
+    public BoxCollider DinoTrigger;
+    public SphereCollider DangerZone;
+    public bool rushPlayer;
+    public AudioSource DinoSound;
+ 
+    void FixedUpdate()
+    {
+      if (rushPlayer == true)
+        {
+            
+            rb.velocity = new Vector3(0, 0, 20);
+            PlayDinoSound();
+            rushPlayer = false;
+        }
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        
+        Destroy(DinoTrigger);
+        rushPlayer = true;
+    }
+
+    void PlayDinoSound()
+    {
+        Debug.Log("Dino Roar");
+        DinoSound.Play();
+    }
+}
