f35d40ac1ccaecf565f1a1d00c54f9d0f711aa61
Kyle Dixon <90637263+KyleDixonVO@users.noreply.github.com>
2021-11-19 23:03:50 -0400

    Collectible V1.0
    Created NES cart, particle system, and sound.

---
+++ Assets/Scripts/Collectable.cs
@@ -0,0 +1,121 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collectable : MonoBehaviour
+{
+    public bool collected = false;
+    public bool movingDown = false;
+    public float timerMax = 4.0f;
+    public AudioSource sound;
+    public ParticleSystem explosion;
+
+    public Vector3 collectableSize;
+
+    public float bobUp;
+    public float bobDown;
+    public float bobSpeed;
+    public float shrinkRate = -15f;
+    public float growRate = 15f;
+
+    void Start()
+    {
+        bobUp = transform.position.y + 1;
+        bobDown = transform.position.y;
+
+        explosion.transform.position = this.transform.position;
+        collectableSize = this.transform.localScale;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(1.0f, 1.0f, 1.0f, Space.World);
+        Bobbing();
+
+        if (collected == false)
+        {
+            if (transform.localScale.y < collectableSize.y)
+            {
+                transform.localScale += new Vector3(0.1F, .1f, .1f) * growRate * Time.deltaTime;
+
+            }
+        }
+
+        if (collected)
+        {
+            StartTimer();
+
+            if (transform.localScale.y > 0)
+            {
+                transform.localScale += new Vector3(0.1F, .1f, .1f) * shrinkRate * Time.deltaTime;
+
+                if (transform.localScale.y < 0)
+                {
+
+                    gameObject.GetComponent<MeshRenderer>().enabled = false;
+                    
+                    
+                }
+            }
+            
+        }
+    }
+
+    void StartTimer()
+    {
+        Debug.Log("timer started");
+        timerMax -= Time.deltaTime;
+
+        if (timerMax <= 0.0f)
+        {
+            TimerEnded();
+        }
+    }
+
+    void TimerEnded()
+    {
+        gameObject.GetComponent<MeshRenderer>().enabled = true;
+        gameObject.GetComponent<SphereCollider>().enabled = true;
+        timerMax = 4.0f;
+        collected = false;
+    }
+
+    void Bobbing()
+    {
+        if (movingDown == false)
+        {
+            transform.Translate(Vector3.up * Time.deltaTime, Space.World);
+
+            if (transform.position.y > bobUp)
+            {
+                movingDown = true;
+                
+            }
+        }
+        else
+        {
+            transform.Translate(Vector3.down * Time.deltaTime, Space.World);
+
+            if (transform.position.y < bobDown)
+            {
+                movingDown = false;
+
+            }
+        }
+
+        
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            sound.Play(0);
+            explosion.Emit(10);
+            gameObject.GetComponent<SphereCollider>().enabled = false;
+            collected = true;
+        }
+    }
+}
