f589ec72fef5c532152a3d389a987dffa9ad8376
coaltic <glindsay156@gmail.com>
2021-11-19 23:11:30 -0400

    [ADDED] GameManager and ButtonManager

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager control;
+
+    public enum State
+    {
+        inMenu,
+        inGamePlay,
+        inPause,
+    }
+
+    public State gameState;
+
+    void Awake()
+    {
+        if (control == null)
+        {
+            DontDestroyOnLoad(gameObject);
+            control = this;
+        }
+        else if (control != this)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+
+    void Start()
+    {
+        SwitchState(State.inMenu);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        switch (gameState)
+        {
+            case State.inMenu:
+                Debug.Log("State: " + gameState);
+                break;
+
+            case State.inGamePlay:
+                Debug.Log("State: " + gameState);
+                break;
+
+            case State.inPause:
+                Debug.Log("State: " + gameState);
+                break;
+        }
+    }
+
+    public void SwitchState(State newState)
+    {
+        gameState = newState;
+    }
+}
